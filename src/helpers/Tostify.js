77b2e782a3078412c4e35bd33edc973a0c5e6590
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-01-05 19:23:18 +0300

    tostify

---
+++ src/helpers/Tostify.js
@@ -0,0 +1,38 @@
+import { toast } from "react-toastify";
+
+export const success = (msg) => {
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+export const errorMsg = (msg) => {
+  toast.error(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+export const warn = (msg) => {
+  toast.warn(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
